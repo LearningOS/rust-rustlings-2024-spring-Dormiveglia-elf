e894dfd037dddfe5ecbd2f9335deac170eb543ac
Zhenyu <81767213+Dormiveglia-elf@users.noreply.github.com>
2024-04-09 13:33:32 +0000

    30 left

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -10,12 +10,14 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        let a = 8;
+        let b = 24;
+        assert!(!(a == b));
     }
 }
